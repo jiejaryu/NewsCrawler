a771e3e28807ad36328ba60947d6943c38bbce8c
Jay Yu <jiejar@gmail.com>
2019-08-25 00:33:25 +0800

    Add for-loop to get news links

--- news.py
+++ news.py
@@ -2,26 +2,27 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
  
- 
+# get website content
 url = 'https://news.google.com/topics/CAAqJQgKIh9DQkFTRVFvSUwyMHZNR3QwTlRFU0JYcG9MVlJYS0FBUAE?hl=zh-TW&gl=TW&ceid=TW%3Azh-Hant'
 r = requests.get(url)
 web_content = r.text
-
 
 soup = BeautifulSoup(web_content,'lxml')
 
 a_tags = soup.find_all('a',class_='DY5T1d')
 
-# print(a_tags)
-
-titleList = []
 
 # print news titles
+titleList = []
+
 for tag in a_tags:
   titleList = titleList + [tag.string]
-
 print(titleList)
 
 
-# titleList = [tag.text for tag in a_tags]
-# print(titleList)
+# print news links
+newsList = []
+
+for tag in a_tags:
+	newsList = newsList + [tag.get('href')]
+print(newsList)
